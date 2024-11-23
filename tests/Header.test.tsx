e62b91be64651c793e36f71f7258faa479d6a350
Lucas Costa <englucascbmelo@gmail.com>
2024-11-22 21:53:30 -0300

    alterando nome da variável

--- tests/Header.test.tsx
+++ tests/Header.test.tsx
@@ -7,9 +7,9 @@ import '@testing-library/jest-dom';
 describe('Header Component', () => {
   it('deve renderizar o logo corretamente', () => {
     render(<Header />);
-    const xablau = screen.getByTestId('logo_lacrei_header');
+    const logo = screen.getByTestId('logo_lacrei_header');
     
-    expect(xablau).toBeInTheDocument();
+    expect(logo).toBeInTheDocument();
   });
 
 });
